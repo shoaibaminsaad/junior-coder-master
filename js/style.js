20daa8b653b5211210497010ed671c6587b2d5b7
shoaibaminsaad <shoaibaminsaad@gmail.com>
2021-07-20 10:00:42 +0600

    Final Added

--- js/style.js
+++ js/style.js
@@ -21,9 +21,9 @@ const date = `${now.getDate()}`;
 const year = `${now.getFullYear()}`;
 const hours = `${now.getHours()}`;
 const minutes = `${now.getMinutes()}`;
-const seconds = `${now.getSeconds()}`;
+// const seconds = `${now.getSeconds()}`;
 
-fullDate.innerHTML = ` ${hours} : ${minutes} : ${seconds} <br> ${year}, ${date} ${month}`;
+fullDate.innerHTML = ` ${hours} : ${minutes} <br> ${year}, ${date} ${month}`;
 
 
 particlesJS("particles-js", {
